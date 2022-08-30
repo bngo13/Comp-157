26deccd356f3c65508898269ea9bd510e828a608
Brain <dalamamommy@dalamamaggot.localdomain>
2022-08-30 14:29:51 -0700

    new file:   Exercise 1/Euclid.rs

---
+++ Exercise 1/CICA.rs
@@ -0,0 +1,20 @@
+///*
+/// 
+/// Consecutive integer checking algorithms
+/// 
+///  */
+
+use std::time::{Duration, Instant};
+
+fn main() {
+    let val1: usize = 3465987;
+    let mut val2: usize = 234;
+
+    let t = if val1 < val2 { val1 } else { val2 };
+
+    
+}
+
+fn CICA() {
+    
+}
