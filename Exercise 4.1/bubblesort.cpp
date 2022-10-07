8b3b926b69db12223d6a8227e2c858421f428dbc
bngo13 <b_ngo13@u.pacific.edu>
2022-10-07 02:21:05 -0700

    Finished Coding portion of Exercise 4.1

---
+++ Exercise 4.1/bubblesort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+    bool isSorted = false;
+
+    while (!isSorted) {
+        isSorted = true;
+        for (int i = 1; i < size; i++) {
+            if (arr[i] < arr[i - 1]) {
+                isSorted = false;
+                int temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+            }
+        }
+    }
+    
+}
+
+void printArr(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+void generateArr(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100;
+    }
+}
+
+int main() {
+
+    srand(0);
+
+    int size = 10000;
+    int* arr = new int[size];
+
+    generateArr(arr, size);
+
+    auto start = chrono::high_resolution_clock::now();
+    bubbleSort(arr, size);
+    auto end = chrono::high_resolution_clock::now();
+
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end-start);
+
+    cout << "Bubble took: " << duration.count() << "ms" << endl;
+
+    return 0;
+}
