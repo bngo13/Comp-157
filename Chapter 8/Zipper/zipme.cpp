87658ee59b11d19f749f0033977077aae82f7430
bngo13 <b_ngo13@u.pacific.edu>
2022-12-02 22:15:28 -0800

    Finished Zipper and it's analysis

--- Chapter 8/Zipper/zipme.cpp
+++ Chapter 8/Zipper/zipme.cpp
@@ -1,36 +1,46 @@
+#include <stdio.h>
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <vector>
+#include <cstring>
+#include <string>
 
 using namespace std;
+
+// Used for file loading
+int inputCount;
+string* allInputs;
+
+// Used for the actual algorithm
 vector<char> firstWord;
 vector<char> secondWord;
 vector<char> thirdWord;
 string testWord;
 
-bool isWordEqual(vector<char> inputWord) {
-    if (inputWord.empty() || inputWord[0] != testWord[0]) {
-        return false;
-    }
-    return true;
+void removeFirstLetter(vector<char> &inWord) {
+    inWord.erase(inWord.begin());
+    testWord.erase(testWord.begin());
 }
 
 bool zipper() {
+    // Length changes each rep so we need a constant
     int length = testWord.length();
-    vector<char> *vectorPointer;
+
     for (int i = 0; i < length; i++) {
         char currentLetter = testWord[0];
 
+        // Refractor to avoid multiple calls to an array
+        char firstWordLetter = firstWord.empty()? ' ' : firstWord.at(0);
+        char secondWordLetter = secondWord.empty()? ' ' : secondWord.at(0);
+        char thirdWordLetter = thirdWord.empty()? ' ' : thirdWord.at(0);
+
         // Test currenLetter against thirdWord's first letter if not empty
-        if (!thirdWord.empty() && thirdWord.at(0) == currentLetter) {
-            thirdWord.erase(thirdWord.begin());
-            testWord.erase(testWord.begin());
-            continue;
+        if (!thirdWord.empty() && thirdWordLetter == currentLetter) {
+            removeFirstLetter(thirdWord);
         }
 
         // Test currentLetter against both firstWord and secondWord for duplicates
-        if (!firstWord.empty() && !secondWord.empty() && firstWord.at(0) == currentLetter && secondWord.at(0) == currentLetter) {
+        else if (!firstWord.empty() && !secondWord.empty() && firstWordLetter == currentLetter && secondWordLetter == currentLetter) {
             // Append currentLetter to the end of the array
             thirdWord.push_back(currentLetter);
 
@@ -38,25 +48,21 @@ bool zipper() {
             firstWord.erase(firstWord.begin());
             secondWord.erase(secondWord.begin());
             testWord.erase(testWord.begin());
-            continue;
         }
 
         // Test currentLetter against firstWord's first letter if not empty
-        if (!firstWord.empty() && firstWord.at(0) == currentLetter) {
-            firstWord.erase(firstWord.begin());
-            testWord.erase(testWord.begin());
-            continue;
+        else if (!firstWord.empty() && firstWordLetter == currentLetter) {
+            removeFirstLetter(firstWord);
         }
 
         // Test currentLetter against secondWord's first letter if not empty
-        if (!secondWord.empty() && secondWord.at(0) == currentLetter) {
-            secondWord.erase(secondWord.begin());
-            testWord.erase(testWord.begin());
-            continue;
+        else if (!secondWord.empty() && secondWordLetter == currentLetter) {
+            removeFirstLetter(secondWord);
         }
 
-        // If all above is false, break out
-        break;
+        else {
+            break;
+        }
     }
 
     /*  Yes state is when:
@@ -69,30 +75,56 @@ bool zipper() {
     return false;
 }
 
-void loadInput(string word, vector<char> *vector) {
-    for (int i = 0; i < word.length(); i++) {
-        (*vector).push_back(word[i]);
+void loadInput(string wordToLoad, vector<char> &vectorToLoadInto) {
+    /* Function
+     * wordToLoad = word to load into
+     * vectorToLoadInto = vector to load word into
+     */
+    vectorToLoadInto.clear();
+    for (int i = 0; i < wordToLoad.length(); i++) {
+        vectorToLoadInto.push_back(wordToLoad[i]);
     }
 }
 
-void getInput() {
-    string word1, word2;
-
-    ifstream input("b.in");
-
-    if ( input.is_open() ) {
-        getline(input, word1);
-        getline(input, word2);
-        getline(input, testWord);
+string* splitInput(string inputWord) {
+    string* splitWord = new string[3];
+    char* inWord = strdup(inputWord.c_str());
+    splitWord[0] = std::strtok(inWord, " ");
+    for (int i = 1; i < 3; i++) {
+        splitWord[i] = std::strtok(NULL, " ");
     }
-    cout << "Inputted Values Are: " << word1 << " & " << word2 << " to match with: " << testWord << endl << endl;
-    loadInput(word1, &firstWord);
-    loadInput(word2, &secondWord);
+
+    return splitWord;
+}
+
+void readInput() {
+    ifstream inFile("b.in");
+    string temp; // used for reading and concats
+    string toBeInputs;
+
+    if (inFile.is_open()) {
+        getline(inFile, temp);
+        inputCount = stoi(temp);
+
+        allInputs = new string[inputCount];
+        for (int i = 0; i < inputCount ; i++) {
+            getline(inFile, allInputs[i]);
+        }
+    }
 }
 
 int main() {
-    getInput();
-    string status = zipper()? "yes": "no";
-    cout << "Data set n: " << status << endl;
+    readInput();
+
+    for (int i = 0; i < inputCount; i++) {
+        string* splitWords = splitInput(allInputs[i]);
+        loadInput(splitWords[0], firstWord);
+        loadInput(splitWords[1], secondWord);
+        testWord = splitWords[2];
+        string status = zipper()? "yes": "no";
+        printf("Data set %i: %s\n", i + 1, status.c_str());
+
+    }
+
     return 0;
 }
